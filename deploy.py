749b774a35b532952e615c4f552d74478a715f85
Phil <pgbson@gmail.com>
2017-03-22 16:50:38 -0400

    added option to enter variables at command line instead of being prompted (for automation support)

--- deploy.py
+++ deploy.py
@@ -14,6 +14,8 @@ parser.add_argument('-pn','--private_networking',help='Private networking (switc
 parser.add_argument('-s','--slug', help='Unique ram size slug', type=str, required=True)
 parser.add_argument('-b','--backups', help='Enable backups (switch)',action='store_true')
 parser.add_argument('-t','--tags', help='tags in tag,tag format', type=str)
+parser.add_argument('-v','--vars',
+				help='Variables for cloud_init scripts in "var=data,var=data" format',type=str)
 config = configparser.ConfigParser()
 args = parser.parse_args()
 try:
@@ -26,20 +28,36 @@ env = Environment(loader=FileSystemLoader(config['config']['cf_templates']))
 
 def main():
 	#break this and see what error it produces and handle it
-	if len(args.tags) > 1:
+	if args.tags:
 		try:
 			args.tags = args.tags.split(',')
 		except ValueError:
 			print('Tags formatted incorrectly, must use "tag,tag"' )
+			sys.exit()
+
+	if args.vars:
+		if ',' in args.vars:
+			try: 
+				args.vars = args.vars.split(',')
+				cloud_var = split_args('list')
+			except:
+				print('Vars formatted incorrectly, must use "var=data, var=data" format')
+				sys.exit()
+		else:
+			cloud_var = split_args('single')
+
+		user_data = cloud_config(cloud_var=cloud_var)
+	else:
+		user_data = cloud_config()	
+
 
 	if args.cloud_config:
-		user_data = cloud_config()
-		create_droplet(args, user_data = user_data)
+		create_droplet(user_data=user_data)
 	else:
-		create_droplet(args)
+		create_droplet()
 
 
-def create_droplet(args,**kwargs):
+def create_droplet(**kwargs):
 	headers = {'Content-Type':'application/json','Authorization':'Bearer '+config['config']['dotoken']}
 	data = {'name':args.name,'region':args.region,'size':args.slug,'image':args.operating_system}
 	if args.cloud_config:
@@ -66,19 +84,37 @@ def create_droplet(args,**kwargs):
 
 
 #Read cloud config file into var
-def cloud_config():
+def cloud_config(**kwargs):
 	try:	
 			data = {}
 			source = env.loader.get_source(env, args.cloud_config+'.yml')[0]
 			parsed = env.parse(source)
 			for var in meta.find_undeclared_variables(parsed):
-				data[var] = input('Enter data for {var}: '.format(var=var))
+				if len(args.vars) > 1:
+					data[var] = kwargs['cloud_var'][var]
+				else:
+					data[var] = input('Enter data for {var}: '.format(var=var))
 			user_data = env.get_template(args.cloud_config+'.yml')
 			user_data = user_data.render(data)
+			print(user_data)
 			return user_data
 
-	except IOError:
+	except IOError as e:
+		#print(e)
 		print('Could not open cloud config file')
 		sys.exit()
+
+def split_args(split_type):
+	cloud_var = {}
+	if split_type =='list':
+		for var in args.vars:
+			pair = var.split('=')
+			cloud_var[pair[0]] = pair[1]
+	else:
+		pair = args.vars.split('=')
+		cloud_var[pair[0]] = pair[1]	
+	return cloud_var
+
+
 
 main()
